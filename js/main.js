53be726b72902e2138f9fdf207fac2293f7ab275
Adam Mann <aemann2@gmail.com>
2021-03-08 19:44:00 -0800

    finished slider

--- js/main.js
+++ js/main.js
@@ -35,9 +35,20 @@ window.onscroll = () => {
 // photo slider
 
 const slider = document.querySelector('.arrow-slider');
+const photo = document.querySelector('.slider-photo');
 
 function sliderHandler() {
-  console.log('click');
+  // adding a closure so count isn't in the global namespace
+  let count = 1;
+  function inner() {
+    if (count < 4) {
+      count++;
+    } else {
+      count = 1;
+    }
+    photo.src = `./images/slider/img${count}.jpg`;
+  }
+  return inner;
 }
 
-slider.addEventListener('click', sliderHandler);
+slider.addEventListener('click', sliderHandler());
